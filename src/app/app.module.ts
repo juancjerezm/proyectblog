8bb29880487489bb1f00e4e13d0794e8ca016086
juancjerezm <juanchob612@gmail.com>
2021-11-12 23:18:40 -0500

    Empezando con los botones de Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [AppComponent, CreatePostComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
