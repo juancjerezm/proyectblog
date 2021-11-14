d7bdb1f5c2d8ed69fc71409f06abb8cefefe4fb6
juancjerezm <juanchob612@gmail.com>
2021-11-14 17:42:27 -0500

    adding material and some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//Librerias de angular
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreatePostComponent } from './create-post/create-post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Componentes del aplicativo
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CreatePostComponent } from "./create-post/create-post.component";
 
-import {MatButtonModule} from '@angular/material/button';
-
-
+//Componentes de Material
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [AppComponent, CreatePostComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'create-post',
-  templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+  selector: "create-post",
+  templateUrl: "./create-post.component.html",
+  styleUrls: ["./create-post.component.css"],
 })
 export class CreatePostComponent implements OnInit {
+  content: string = "Hola Mundo";
+  texto: string = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showText() {
+    this.content = this.texto;
   }
-
 }
